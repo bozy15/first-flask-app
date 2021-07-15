37953df2fe5ef1071d796dd43d6736fdd2f10e6e
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-07-15 13:50:52 +0100

    Add new html file and creates a route to read members bio

--- run.py
+++ run.py
@@ -1,34 +1,46 @@
 import os
 import json
-from flask import Flask, render_template 
+from flask import Flask, render_template
 
-app = Flask(__name__) # Standard Flask app
+app = Flask(__name__)  # Standard Flask app
 
 
-@app.route("/") # Standard Flask app route
+@app.route("/")  # Standard Flask app route
 def index():
     return render_template("index.html")
 
 
-@app.route("/about") # Standard Flask app route
-def about(): 
-    data = [] 
+@app.route("/about")  # Standard Flask app route
+def about():
+    data = []
     with open("data/company.json", "r") as json_data:
-        data = json.load(json_data) 
+        data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact") # Standard Flask app route
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:  # Open the JSON file for reading
+        data = json.load(json_data)  # Read the JSON into the buffer
+        for obj in data:  # Iterate over the list of dicts
+            if obj["url"] == member_name:  # Find the member in the list
+                member = obj  # Store the member's dict
+    return render_template("member.html", member=member)  # And render the results
+
+
+@app.route("/contact")  # Standard Flask app route
 def contact():
     return render_template("contact.html", page_title="Contact Us")
 
 
-@app.route("/careers") # Standard Flask app route
+@app.route("/careers")  # Standard Flask app route
 def careers():
     return render_template("careers.html", page_title="Careers")
 
+
 # run the app
-if __name__ == "__main__": 
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
